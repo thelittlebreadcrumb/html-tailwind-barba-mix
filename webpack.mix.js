a87b320e153b33b0ef853d9f845fd3547cecb82c
Bhavya Daiya <bhavya@bhavzproject.com>
2021-10-20 06:57:12 +1300

    hot reload fix fail

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,34 @@
 // 
 
 let mix = require('laravel-mix');
+let path = require('path')
 
-mix.js("src/app.js", "public/js")
-    .postCss("src/app.css", "public/css", [
-        require("tailwindcss"),
-    ]);
+mix.setPublicPath(path.resolve('./'));
+
+mix.js("src/app.js", "public/js");
+
+mix.postCss('src/app.css', 'public/css/app.css', [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('postcss-nested'),
+    require('postcss-focus-visible'),
+    require('autoprefixer'),
+])
+
+mix.options({
+    cssNano: { minifyFontValues: false }
+});
 
 mix.browserSync({
-    watch: true,
-    proxy: 'wbp.test',
-});    
+    server: './public/',
+    files: [
+        'src/**/*.html', 
+        'src/**/*.css', 
+        'public/**/*.(css|js)', 
+    ],
+    // Option to open in non default OS browser.
+    // browser: "firefox",
+    notify: true
+})
+
+mix.version()
